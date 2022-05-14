599cbb7d4ae76b57a55c0c48e0be29355f08565e
William Reiff <wrreiff@gmail.com>
2022-05-14 09:28:39 -0400

    minor check

--- background.js
+++ background.js
@@ -1,1 +1,7 @@
 console.log('this is the background script')
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log(request.domTree);
+    sendResponse();
+  });
--- extension/js/content.js
+++ extension/js/content.js
@@ -1,1 +1,27 @@
-console.log("This is the content script")
+console.log("This is the content script");
+
+// const domTree = window.document.createTreeWalker(
+//     window.document.body,
+//     NodeFilter.SHOW_ELEMENT
+// );
+
+const domTree = window.document.querySelectorAll('*');
+console.log(JSON.stringify(window.document.querySelectorAll('*')));
+
+// const nodeList = [];
+// const nodeDetails = [];
+// let currentNode = domTree.currentNode;
+
+// while (currentNode) {
+//     nodeList.push(String(currentNode.outerHTML));
+//     nodeDetails.push(currentNode);
+//     currentNode = domTree.nextNode();
+// }
+
+const test = JSON.stringify(domTree);
+// console.log(nodeDetails);
+// console.log(nodeList);
+
+console.log(test)
+
+chrome.runtime.sendMessage({domTree: domTree});
